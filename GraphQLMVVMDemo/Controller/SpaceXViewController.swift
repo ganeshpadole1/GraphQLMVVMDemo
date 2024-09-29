abc5b29713ca21b0542d75e92bec3ba3ff8fa133
ganesh padole <ganeshpadole55@gmail.com>
2024-09-29 17:15:39 +0530

    initial pod setup

---
+++ GraphQLMVVMDemo/Controller/SpaceXViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXViewController.swift
+//  GraphQLMVVMDemo
+//
+//  Created by Ganesh Padole on 29/09/24.
+//
+
+import UIKit
+
+class SpaceXViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "SpaceX"
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GraphQLMVVMDemo' do
+# Comment the next line if you don't want to use dynamic frameworks
+use_frameworks!
+
+# Pods for GraphQLMVVMDemo
+pod "Apollo"
+end
